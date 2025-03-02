3e2551b7c90772543c65cee1d140cea6f3c3eefe
makurbenjamin <makurbenjamin@gmail.com>
2025-03-02 17:00:03 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,12 @@
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/service-worker.js')
-        .then((registration) => {
-          console.log('Service Worker registered: ', registration);
-        })
-        .catch((error) => {
-          console.log('Service Worker registration failed: ', error);
-        });
-    });
-  }
+// public/service-worker.js
+self.addEventListener('push', (event) => {
+  const payload = event.data ? event.data.text() : 'No payload';
+  const options = {
+    body: payload,
+    icon: '/icon.png', // Ensure this path is correct
+  };
+
+  event.waitUntil(
+    self.registration.showNotification('Push Notification', options)
+  );
+});
