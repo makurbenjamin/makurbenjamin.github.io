8e4581b853b32b5441e2317edfc3d7574f266909
makurbenjamin <makurbenjamin@gmail.com>
2025-03-02 21:38:31 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,13 @@
-// public/service-worker.js
 self.addEventListener('push', (event) => {
-  const payload = event.data ? event.data.text() : 'No payload';
-  const options = {
-    body: payload,
-    icon: '/icon.png', // Ensure this path is correct
-  };
+  const data = event.data ? JSON.parse(event.data.text()) : { title: 'New Notification', body: 'No payload provided.' };
 
   event.waitUntil(
-    self.registration.showNotification('Push Notification', options)
+    self.registration.showNotification(data.title, {
+      body: data.body,
+      icon: '/icon.png', // Path to the icon file
+      badge: '/badge.png', // Path to the badge file
+      vibrate: [200, 100, 200], // Optional: Vibration pattern
+      requireInteraction: true, // Optional: Keep the notification open until the user interacts with it
+    })
   );
 });
